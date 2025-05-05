1562ef22bf1dc4ee0140651953920e70679843c4
ACE <202493@mail.ru>
2025-05-05 15:14:48 +0300

    removed bootstrap

--- src/components/main1.tsx
+++ src/components/main1.tsx
@@ -1,15 +1,4 @@
-//import { useState } from "react";
-import CardExample from "../bootstrap/card";
-//import List from "../bootstrap/list";
-//import Spinners from "../bootstrap/spinners";
-import BasicTable from "../bootstrap/table";
-//import BasicToast from "../bootstrap/toast";
-//import Example from "../bootstrap/xxx";
-import BasicTabs from "./../bootstrap/tabs";
-//import ProgressBarContainer from "./progressBarContainer";
 import CustomKnob from "./customKnob";
-//import CustomKnob1 from "./customKnob0";
-import CustomKnob2 from "./customKnob2";
 import { observer } from "mobx-react-lite";
 import viewStore from "../store/viewStore";
 import MyForm from "./machineForm";
@@ -88,8 +77,7 @@ const Main1 = observer(() => {
 						</div>	
 						<div className="col-12 h-50 p-3">
 							<div className="vidget col-12 h-100">
-								{/* <CardExample /> */}
-							</div>
+ 							</div>
 						</div>					
 					</div>
 
@@ -97,8 +85,7 @@ const Main1 = observer(() => {
 						<div className="col-12 h-100 p-3">
 							<div className="vidget col-12 h-100">
 								<div className="w-100 h-100 d-flex align-items-center justify-content-center">
-									{/* <CustomKnob1 /> */}
-								</div>
+ 								</div>
 
 							</div>
 						</div>				
@@ -107,13 +94,11 @@ const Main1 = observer(() => {
 					<div className="d-flex flex-column h-100 col-4 ">
 						<div className="col-12 h-60 p-3">
 							<div className="vidget col-12 h-100">
-{/* 								<CustomKnob2 />
- */}							</div>
+							</div>
 						</div>	
 						<div className="col-12 h-40 p-3">
 							<div className="vidget col-12 h-100">
-{/* 								<BasicTable />
- */}							</div>
+							</div>
 						</div>	
 						
 					</div>						
@@ -122,8 +107,7 @@ const Main1 = observer(() => {
 				<div className="d-flex row h-15 p-3">
 					<div className="col-12 h-100">
 						<div className="vidget col-12 h-100">
-{/* 							<BasicTabs />
- */}						</div>
+						</div>
 					</div>					
 				</div>
 			</div>
