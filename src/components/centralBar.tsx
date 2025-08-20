6ab4e2d2d8f89a2b03ed337cbfb680cd051bd6bd
ACE <202493@mail.ru>
2025-08-20 13:36:54 +0300

    added midBar

--- src/components/centralBar.tsx
+++ src/components/centralBar.tsx
@@ -1,17 +1,17 @@
 import LeftBar from "./leftBar";
 import ProgressBarContainer from "./progressBarContainer";
 import RightBar from "./rightBar";
+import MidBar from "./midBar";
 
 const CentralBar = () => {
-
-
 	return (
 
 		<div id="CentralBar" className="d-flex  w-100 h-100 flex-start">
 			<div className="d-flex w-100 h-100 flex-start flex-column">
 				<ProgressBarContainer />
-				<div className="d-flex w-100 h-100 flex-start ">
+				<div className="d-flex w-100 h-100 flex-start">
 					<LeftBar />
+					<MidBar />					
 				</div>
 			</div>
 			<RightBar />
---
+++ src/components/midBar.tsx
@@ -0,0 +1,109 @@
+
+
+const MidBar = () => {
+	let params = [
+		{ name: 'N2', measure: 'bar', val: 4.8 },
+		{ name: 'Nd', measure: 'mm', val: 12.7 },
+		{ name: 'f', measure: 'kHz', val: 88.7 },
+	]
+
+	let paramsLimit = [
+		{ name: 'X', measure: 'mm', val: 1250.44 },
+		{ name: 'Y', measure: 'mm', val: 44.777 },
+		{ name: 'Z', measure: 'mm', val: 28.77 },
+	]
+
+
+	return (
+
+
+		<div className="d-flex flex-column">
+			<div className="d-flex mx-2 flex-wrap">
+				{
+					paramsLimit.map((item, i) => {
+						return (
+							<div className="currentPlanMeasureWpapperWpapper d-flex" key={i}>
+								<div className="currentPlanMeasureWpapper">
+									<div className="currentPlanMeasure">
+
+										<div className="currentPlanMeasureNameCont">
+											<div className="currentPlanMeasureName">
+												{item.name}
+											</div>
+										</div>
+
+										<div className="currentPlanMeasureValConainer d-flex align-items-center">
+											<div className="currentPlanMeasureValue">
+												{item.val.toFixed(2)}
+											</div>
+											<div className="currentPlanMeasureItem mx-1">
+												{item.measure}
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className="currentPlanLimits my-2 ms-2">
+									<div className="d-flex flex-column">
+										<div className="d-flex  align-items-center mb-1">
+											<div className="led-green-medium">
+											</div>
+											<div className="limitText">
+												Limit-
+											</div>
+										</div>
+										<div className="d-flex  align-items-center">
+											<div className="led-gray-medium">
+											</div>
+											<div className="limitText">
+												Limit+
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+						)
+
+					})
+				}
+			</div>
+			<div className="d-flex mx-2 flex-wrap">
+				{
+					params.map((item, i) => {
+						return (
+							<div className="currentPlanMeasureWpapperWpapper d-flex" key={i}>
+								<div className="currentPlanMeasureWpapper">
+									<div className="currentPlanMeasure">
+
+										<div className="currentPlanMeasureNameCont">
+											<div className="currentPlanMeasureName">
+												{item.name}
+											</div>
+										</div>
+
+										<div className="currentPlanMeasureValConainer d-flex align-items center">
+											<div className="currentPlanMeasureValue">
+												{item.val.toFixed(2)}
+											</div>
+											<div className="currentPlanMeasureItem mx-1">
+												{item.measure}
+											</div>
+										</div>
+									</div>
+								</div>
+								<div className="currentPlanLimits">
+								</div>
+							</div>
+						)
+
+					})
+				}
+			</div>
+			<div className="d-flex w-100 h-100 flex-start p-3">
+				<img src="/images/06.08 1,5мм-01.svg" alt="Plan Image" />
+			</div>
+		</div>
+
+	)
+};
+
+export default MidBar;
