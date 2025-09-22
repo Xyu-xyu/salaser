176cec84af7a2c1ff226310b8f23717808e45cb8
ACE <202493@mail.ru>
2025-09-22 13:29:52 +0300

    removed trash files

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,6 @@ import './../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 import Main from './components/main.js'
 import { observer } from "mobx-react-lite";
 import viewStore from './store/viewStore.js';
-/* import IosToggle0 from './components/toggles/iosToggle0.js';
-import IosToggle1 from './components/toggles/iosToggle1.js';
-import IosToggle2 from './components/toggles/iosToggle2.js';
-import IosToggle3 from './components/toggles/iosToggle3.tsx'; */
 import { ToastContainer } from 'react-toastify';
 import SvgDefs from './components/svgDef.tsx';
 import ModalGeneric from "./components/modalGeneric.js";
@@ -25,12 +21,6 @@ const App = observer(() => {
 				className={"d-flex flex-column min-vh-100 "+ viewStore.theme}
 				onContextMenu={handleContextMenu}
 			>
-				<div className=''>
-				{/* <IosToggle0 />
-					<IosToggle1 /> 
-					<IosToggle2 />
-					<IosToggle3 />*/}
-				</div>
 				<SvgDefs />
 				<ToastContainer toastStyle={{
 					minWidth: '450px',
